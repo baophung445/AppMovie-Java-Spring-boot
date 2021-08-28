96c6da1e40e0a7ca1cd76f28c1e080ded050de3e
hoang.doan <hoang.doan@mission.plus>
2021-08-29 00:23:24 +0700

    end

---
+++ src/main/resources/static/js/review_video_role.js
@@ -0,0 +1,35 @@
+/*
+
+$(document).ready(
+	function() {
+
+		if (localStorage.getItem('AcceptVideo')) {
+			Swal.fire({
+				position: 'center',
+				icon: 'success',
+				title: 'Bộ phim đã được Accept !',
+				showConfirmButton: false,
+				timer: 500
+			})
+			localStorage.removeItem('AcceptVideo');
+		}
+
+		$('#btnAccept').click(function() {
+			localStorage.setItem('AcceptVideo', 'true');
+		})
+
+		$('#btndelete').click(function() {
+
+			setTimeout(() => {
+				Swal.fire({
+					position: 'center',
+					icon: 'success',
+					title: 'Bộ phim đã được Xóa !',
+					showConfirmButton: false,
+					timer: 500
+				})
+			}, 1000)
+
+		})
+
+	});*/
